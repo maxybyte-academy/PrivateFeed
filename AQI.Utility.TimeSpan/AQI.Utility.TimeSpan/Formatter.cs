2a6c82237035404b85993e0b4a1ad938e8179ee6
maxybyte <maxybyteschool@gmail.com>
2026-02-16 10:44:09 +0200

    update

---
+++ AQI.Utility.TimeSpan/AQI.Utility.TimeSpan/Formatter.cs
@@ -0,0 +1,32 @@
+﻿namespace AQI.Utility.TimeSpan
+{
+    public static class Formatter
+    {
+
+        public static string ToRelativeTimeAgoString(this DateTime occurred)
+        {
+            var timeSpan = DateTime.UtcNow - occurred.ToUniversalTime();
+
+            if (timeSpan.TotalSeconds < 60)
+                return $"{(int)timeSpan.TotalSeconds} second{((int)timeSpan.TotalSeconds != 1 ? "s" : "")} ago";
+
+            if (timeSpan.TotalMinutes < 60)
+                return $"{(int)timeSpan.TotalMinutes} minute{((int)timeSpan.TotalMinutes != 1 ? "s" : "")} ago";
+
+            if (timeSpan.TotalHours < 24)
+                return $"{(int)timeSpan.TotalHours} hour{((int)timeSpan.TotalHours != 1 ? "s" : "")} ago";
+
+            if (timeSpan.TotalDays < 30)
+                return $"{(int)timeSpan.TotalDays} day{((int)timeSpan.TotalDays != 1 ? "s" : "")} ago";
+
+            if (timeSpan.TotalDays < 365)
+            {
+                int months = (int)(timeSpan.TotalDays / 30);
+                return $"{months} month{(months != 1 ? "s" : "")} ago";
+            }
+
+            int years = (int)(timeSpan.TotalDays / 365);
+            return $"{years} year{(years != 1 ? "s" : "")} ago";
+        }
+    }
+}
---
+++ AQI.Utility.TimeSpan/AQI.Utility.TimeSpan/docs/readme.md
@@ -0,0 +1,5 @@
+﻿# AQI.Utility.TimeSpan
+
+Human-friendly "time ago" formatter that converts a `DateTime` into a relative time string.
+
+## Installation
